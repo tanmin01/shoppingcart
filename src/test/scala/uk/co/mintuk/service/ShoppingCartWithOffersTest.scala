e87328e7d60f4130d1d6861ffcca2b4f59b3cf40
UR65MO <min.tan@ing.nl>
2017-03-26 12:10:01 +0200

    working version with out offers

---
+++ src/test/scala/uk/co/mintuk/service/ShoppingCartWithOffersTest.scala
@@ -0,0 +1,21 @@
+package uk.co.mintuk.service
+
+import org.scalatest._
+import uk.co.mintuk.datamodel.{Apple, ShoppingItem, Orange}
+
+
+//as no dependency to any other services so we use only dummy data, not mocked data
+class ShoppingCartWithOffersTest extends FunSpec with ShouldMatchers with BeforeAndAfter with Matchers{
+
+  describe("Simple shopping cart with no offers") {
+    it("an empty shopping cart should pay zero amount") {
+      val noItems : List[ShoppingItem] = List.empty[ShoppingItem]
+      shoppingCart.calculatePaymentByGroupWithOffers(noItems) should equal(0)
+    }
+
+    it("an shopping cart with some items should pay some amount") {
+      val someItems: List[ShoppingItem] = List(Apple(),Apple(),Orange(),Apple())
+      shoppingCart.calculatePaymentByGroupWithOffers(someItems) should equal(0)
+    }
+  }
+}
