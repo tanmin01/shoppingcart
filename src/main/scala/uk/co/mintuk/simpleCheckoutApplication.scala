d5c57921baf73488c747cdf4554d5aebb717e889
UR65MO <min.tan@ing.nl>
2017-03-26 10:53:04 +0200

    working version with out offers

--- src/main/scala/uk/co/mintuk/simpleCheckoutApplication.scala
+++ src/main/scala/uk/co/mintuk/simpleCheckoutApplication.scala
@@ -1,15 +1,15 @@
 package uk.co.mintuk
 
-import uk.co.mintuk.datamodel.{Apple, Orange, Payable}
+import uk.co.mintuk.datamodel.{Apple, Orange, ShoppingItem}
 import uk.co.mintuk.service.shoppingCart
 
 object simpleCheckoutApplication {
   def main(args : Array[String]) {
-    val noItems : List[Payable] = List.empty[Payable]
+    val noItems : List[ShoppingItem] = List.empty[ShoppingItem]
     println("I have got no item " + noItems)
     println("I have to pay this amount " + shoppingCart.calculatePayment(noItems) + " pence")
 
-    val someItems: List[Payable] = List(Apple(),Apple(),Orange(),Apple())
+    val someItems: List[ShoppingItem] = List(Apple(),Apple(),Orange(),Apple())
     println("I have got some items " + someItems)
     println("I have to pay this amount " + shoppingCart.calculatePayment(someItems)+ " pence")
   }
